faa48bff6c0a2e5d5c3e7b36e9b22469ef27fe9a
rafaelnepomuceno00 <rafaelnepomuceno00@gmail.com>
2023-06-05 02:24:56 -0300

    trying to fix responsive text

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,12 @@ import theme from "../theme";
 import ResponsiveAppBar from "../components/AppBar";
 import { ThemeProvider } from "@emotion/react";
 import { Container } from "@mui/material";
-import "./style.css";
 import Home from "../components/Home";
 import About from "../components/About";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import useLanguage from "../language";
+import "./style.css";
 
 const IndexPage = () => {
   const { language, toggleLanguage } = useLanguage();
